a0a67b6acd700da76a44889266f9372db23fb245
Bot3939 <39862138+Bot3939@users.noreply.github.com>
2020-03-07 13:30:02 -0300

    selection sort

--- visualizer.py
+++ visualizer.py
@@ -12,6 +12,15 @@ def insertion_sort(l):
             yield l
         l[j+1] = k
 
+def selection_sort(l):
+    for i in range(len(l)):  
+        min_ind = i 
+        for j in range(i+1, len(l)):
+            if l[j] < l[min_ind]:
+                min_ind = j
+        l[i], l[min_ind] = l[min_ind], l[i]
+        yield l
+
 #generate array, window height and width and the empty canvas for drawing with an
 #empty copy to reset it every frame
 arr = [randint(0, 500) for i in range(0, 50)]
@@ -20,11 +29,13 @@ WIDTH = len(arr) * 10
 img = np.zeros((HEIGHT, WIDTH, 3), np.uint8)
 reset = img.copy()
 
-for step in insertion_sort(arr):
+steps = selection_sort(arr)
+
+for step in steps:
     for i in range(len(arr)):
         img[HEIGHT:HEIGHT-arr[i]:-1, i*10:i*10+5] = 255, 255, 255
         cv2.imshow('', img)
-    cv2.waitKey(1)
+    cv2.waitKey(10)
     img = reset.copy()
 
 cv2.waitKey(0)
