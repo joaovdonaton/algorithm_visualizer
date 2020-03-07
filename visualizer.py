48deaee4ca52781303fe1128d771589eafb1f3fb
Bot3939 <39862138+Bot3939@users.noreply.github.com>
2020-03-07 18:13:01 -0300

    red color shows changes in between frames

--- visualizer.py
+++ visualizer.py
@@ -37,20 +37,35 @@ def bogo_sort(l):
 
 #generate array, window height and width and the empty canvas for drawing with an
 #empty copy to reset it every frame
-arr = [randint(0, 500) for i in range(0, 30)]
+arr = [randint(0, 500) for i in range(0, 100)]
 HEIGHT = max(arr)+1
 WIDTH = len(arr) * 10
 img = np.zeros((HEIGHT, WIDTH, 3), np.uint8)
 reset = img.copy()
 
+#call bogo_sort, selection_sort or insertion_sort here with the array
+#as a parameter
 steps = insertion_sort(arr)
 
+#prev_step stores the array before the current "step" in the sorting algorithms
+#step = whenever the algorithm changes the array
+#different is a list of indexes that have changed since the previous step
+prev_step, different = arr, []
 for step in steps:
     for i in range(len(arr)):
-        img[HEIGHT:HEIGHT-arr[i]:-1, i*10:i*10+5] = 255, 255, 255
+        if step[i] != prev_step[i]:
+            different.append(i)
+
+    for i in range(len(arr)):
+        if i in different:
+            img[HEIGHT:HEIGHT-arr[i]:-1, i*10:i*10+5] = 0, 0, 200
+        else:
+            img[HEIGHT:HEIGHT-arr[i]:-1, i*10:i*10+5] = 255, 255, 255
         cv2.imshow('', img)
-    cv2.waitKey(10)
+    cv2.waitKey(1)
     img = reset.copy()
+    #reset different and set prev_step
+    prev_step, different = step.copy(), []
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
