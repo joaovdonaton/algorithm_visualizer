b3a36f695b898c264149677426afc1adec120821
Bot3939 <39862138+Bot3939@users.noreply.github.com>
2020-03-07 13:13:32 -0300

    working visualization example

--- visualizer.py
+++ visualizer.py
@@ -2,24 +2,24 @@ import cv2
 import numpy as np
 from random import randint
 
-def insertion_sort(l):
-    for index in range(1, len(l)):
-        k = l[index]
-        j = index-1
-        while j >= 0 and k < l[j]:
-            l[j+1] = l[j]
-            j-=1
-        l[j+1] = k
+arr = [randint(0, 500) for i in range(0, 100)]
+HEIGHT = max(arr)+1
+WIDTH = len(arr) * 10
+img = np.zeros((HEIGHT, WIDTH, 3), np.uint8)
+reset = img.copy()
 
-    return l
-
-arr = [randint(0, 300) for i in range(0, 25)]
-img = np.zeros((500, 500, 3), np.uint8)
-
-for i in range(len(arr)):
-    img[499:499-arr[i]:-1, i*10:i*10+5] = 255, 255, 255
-
-cv2.imshow('', img)
+for index in range(1, len(arr)):
+    k = arr[index]
+    j = index-1
+    while j >= 0 and k < arr[j]:
+        arr[j+1] = arr[j]
+        j-=1
+        for i in range(len(arr)):
+            img[HEIGHT:HEIGHT-arr[i]:-1, i*10:i*10+5] = 255, 255, 255
+            cv2.imshow('', img)
+        cv2.waitKey(1)
+        img = reset.copy()
+    arr[j+1] = k
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
